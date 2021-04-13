1e67a53e58b3b77cd92927cb8ae7048f84b18093
Jonathan Lee <jonathan.wing.lee@gmail.com>
2021-04-12 22:16:38 -0500

    minicssextractplugin to separate out css from js iat buildtime

--- webpack.parts.js
+++ webpack.parts.js
@@ -1,5 +1,6 @@
 const { WebpackPluginServe } = require('webpack-plugin-serve');
 const { MiniHtmlWebpackPlugin } = require('mini-html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 exports.devServer = () => ({
   watch: true,
@@ -22,3 +23,25 @@ exports.loadCSS = () => ({
     rules: [{ test: /\.css$/, use: ['style-loader', 'css-loader'] }],
   },
 });
+
+exports.extractCSS = ({ options = {}, loaders = [] } = {}) => {
+  return {
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          use: [
+            { loader: MiniCssExtractPlugin.loader, options },
+            'css-loader',
+          ].concat(loaders),
+          sideEffects: true,
+        },
+      ],
+    },
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: '[name].css',
+      }),
+    ],
+  };
+};
